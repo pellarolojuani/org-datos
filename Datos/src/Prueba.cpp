d2d94445b0cb8fbbe22045abab91b6cdce31cc63
lucianasl@gmail.com <lucianasl@gmail.com@d1e0b4a9-a9fa-b758-e854-71e3184de9cd>
2013-03-22 02:56:40 +0000

    probando comiteo

---
+++ Datos/src/Prueba.cpp
@@ -0,0 +1,18 @@
+/*
+ * Prueba.cpp
+ *
+ *  Created on: Mar 21, 2013
+ *      Author: lucia
+ */
+
+#include "Prueba.h"
+
+Prueba::Prueba() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Prueba::~Prueba() {
+	// TODO Auto-generated destructor stub
+}
+
