a1eb6687f461a7b3d588f4a96b631a506914b8bd
lucianasl@gmail.com <lucianasl@gmail.com@d1e0b4a9-a9fa-b758-e854-71e3184de9cd>
2013-03-22 02:43:10 +0000

    Wellcame everybody

---
+++ Datos/src/Datos.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Datos.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Goodbye matrix, hello world!" << endl;
+	return 0;
+}
