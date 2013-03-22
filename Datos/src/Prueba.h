981ebf14f2182f8b7835f80cd838b3dbb9f0a2ce
lucianasl@gmail.com <lucianasl@gmail.com@d1e0b4a9-a9fa-b758-e854-71e3184de9cd>
2013-03-22 02:57:09 +0000

    probandoouu

---
+++ Datos/src/Prueba.h
@@ -0,0 +1,17 @@
+/*
+ * Prueba.h
+ *
+ *  Created on: Mar 21, 2013
+ *      Author: lucia
+ */
+
+#ifndef PRUEBA_H_
+#define PRUEBA_H_
+
+class Prueba {
+public:
+	Prueba();
+	virtual ~Prueba();
+};
+
+#endif /* PRUEBA_H_ */
