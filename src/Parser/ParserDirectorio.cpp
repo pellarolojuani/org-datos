024b09e53d342aa5c68ffe25301ba4c4e1ec948b
lucianasl <lucianasl@d1e0b4a9-a9fa-b758-e854-71e3184de9cd>
2013-06-21 19:02:18 +0000

    arreglado para numerar los documentos del 1 al N para no tener problemas con los gamma, despues habría que hacer un descDocumento o algo así

--- src/Parser/ParserDirectorio.cpp
+++ src/Parser/ParserDirectorio.cpp
@@ -18,7 +18,6 @@
 #include <time.h>
 #include <string>
 
-
 static const string CURR_DIR_STR = ".";
 static const string PARENT_DIR_STR = "..";
 
@@ -30,45 +29,44 @@ ParserDirectorio::ParserDirectorio(char* nombreDirectorio) {
 
 	arbolito = new abb::ArbolB<abb::Nodo, ORDEN_NODO>;
 
-	this->archivoDirectorios = fopen(constantes::NombresArchivos::archivoDirectorios, "wb");
-	this->archivoPunteros = fopen(constantes::NombresArchivos::archivoPunteros, "wb");
+	this->archivoDirectorios = fopen(
+			constantes::NombresArchivos::archivoDirectorios, "wb");
+	this->archivoPunteros = fopen(constantes::NombresArchivos::archivoPunteros,"wb");
 	this->archivoLexicoFC = fopen(constantes::NombresArchivos::archivoLexico,"wb");
 	this->tablaLexicoFC = fopen(constantes::NombresArchivos::archivoTablaLexico,"wb");
 	this->archivoPosRelativas = fopen(constantes::NombresArchivos::archivoPosicionesRelativas, "wb");
 	//No se si esta bien, pero creo que cada vez que armo el indice debo borrar todo el gamma anterior.
 	this->archivoGamma = fopen(constantes::NombresArchivos::archivoGamma, "wb");
 
-
 }
 
-void ParserDirectorio::parsearDirectorio(){
+void ParserDirectorio::parsearDirectorio() {
 	this->parsearDirectorioRec(this->nombreDirectorio);
 }
 
-void ParserDirectorio::parsearDirectorioRec(char* directorioRuta){
+void ParserDirectorio::parsearDirectorioRec(char* directorioRuta) {
 
 	DIR *dir;
 	struct dirent *dit;
 
-	if ((dir = opendir(directorioRuta)) == NULL)
-	{
-		cout<<"El directorio "<<dit->d_name<<" no pudo a abrirse."<<endl;
+	if ((dir = opendir(directorioRuta)) == NULL) {
+		cout << "El directorio " << dit->d_name << " no pudo a abrirse."
+				<< endl;
 	}
 
 	int i;
-	int offset=0;
+	int offset = 0;
+	int contadorDir = 1;
 
-
-	if (dir != NULL)
-	{
+	if (dir != NULL) {
 		struct dirent* entry;
-		while ((entry = readdir(dir))){
-			if (!isCurrOrParentDir(entry->d_name)){ // Excluyo "." y ".."
+		while ((entry = readdir(dir))) {
+			if (!isCurrOrParentDir(entry->d_name)) { // Excluyo "." y ".."
 				//Por cada archivo leido
 				parser->resetUltimaPosicion();
 				char* nombreDirectorio = entry->d_name;
 
-				char ruta[1024]="";
+				char ruta[1024] = "";
 				strcat(ruta, directorioRuta);
 				strcat(ruta, "/");
 				strcat(ruta, nombreDirectorio);
@@ -77,17 +75,18 @@ void ParserDirectorio::parsearDirectorioRec(char* directorioRuta){
 				struct stat st;
 				lstat(ruta, &st);
 
-				if(!S_ISDIR(st.st_mode)){
+				if (!S_ISDIR(st.st_mode)) {
 					//Guardo el nombre del archivo en archivoDirectorios.
 					fputs(nombreDirectorio, this->archivoDirectorios);
 					fputs(",", this->archivoDirectorios);
 
 					//TODO variable a codificar en gamma (sería el archivo donde aparece)
-					cout<<"Indexando directorio.."<<nombreDirectorio<<offset<<endl;
+					cout << "Indexando directorio.." << nombreDirectorio
+							<< offset << endl;
 
-
-					parseFile(dirAct, offset);
-					offset+= strlen(nombreDirectorio)+1;
+					parseFile(dirAct, contadorDir);
+					offset += strlen(nombreDirectorio) + 1;
+					contadorDir++;
 
 				} else {
 					parsearDirectorioRec(ruta);
@@ -100,9 +99,9 @@ void ParserDirectorio::parsearDirectorioRec(char* directorioRuta){
 	abb::Nodo nod;
 	//TODO ARREGLAR ESTA GUASADA
 	//lo agregue porque al levantar el arbol misteriosamente no toma la ultima palabra. APCSC.
-	nod.setPalabra("zzzzzzzzzzzzzzzzzzzzz");
+	nod.setPalabra("zzzzzzzzzzzzzzzzzzzzzzz");
 	Posiciones* pos = new parser::Posiciones();
-	pos->agregarPosicion(0);
+	pos->agregarPosicion(1);
 	nod.setDocumentos(pos);
 	nod.setPosiciones(pos);
 	arbolito->insertar(nod);
@@ -119,20 +118,21 @@ void ParserDirectorio::parsearDirectorioRec(char* directorioRuta){
 
 }
 
-void ParserDirectorio::almacenarIndice(){
+void ParserDirectorio::almacenarIndice() {
 	frontcoding::Frontcoding FC(this->archivoLexicoFC, this->tablaLexicoFC);
 	arbolito->guardarLexicoYPunteros(FC);
 }
 
-void ParserDirectorio::parseFile(FILE* dirAct, int offsetDocs){
+void ParserDirectorio::parseFile(FILE* dirAct, int offsetDocs) {
 	//Paso a abrir el directorio y parsearlo linea por linea
 	char linea[LONG_MAX_LINEA];
 	Posiciones posis;
 
-	while (fgets(linea, LONG_MAX_LINEA, dirAct) != NULL){
+	while (fgets(linea, LONG_MAX_LINEA, dirAct) != NULL) {
 		posis.resetCantidadPosiciones();
-		string* parseo = this->parser->parsearLinea(linea,&posis, MAX_POSICIONES_LINEA);
-		for (int i = 0; i<posis.getCantPosiciones(); i++) {
+		string* parseo = this->parser->parsearLinea(linea, &posis,
+				MAX_POSICIONES_LINEA);
+		for (int i = 0; i < posis.getCantPosiciones(); i++) {
 
 			//VEO SI YA ESTA EN EL ABB
 			abb::Nodo nodotar;
@@ -143,13 +143,14 @@ void ParserDirectorio::parseFile(FILE* dirAct, int offsetDocs){
 			if (esta) {
 				nodotar = arbolito->buscarYdevolver(nodotar);
 				//Agrego su nro de palabra:
-				nodotar.getPosiciones()->agregarPosicion(posis.getPosiciones()[i]);
-				nodotar.setFrecuencia(nodotar.getFrecuencia()+1);
+				nodotar.getPosiciones()->agregarPosicion(
+						posis.getPosiciones()[i]);
+				nodotar.setFrecuencia(nodotar.getFrecuencia() + 1);
 				nodotar.getDocumentos()->agregarPosicion(offsetDocs);
 				this->arbolito->modify(nodotar);
 
 			} else {
-			//SI NO ESTA LO INSERTO.
+				//SI NO ESTA LO INSERTO.
 				Posiciones* nuevosOffsetDocs = new Posiciones();
 				Posiciones* nuevasPosiciones = new Posiciones();
 				nuevasPosiciones->agregarPosicion(posis.getPosiciones()[i]);
@@ -163,18 +164,16 @@ void ParserDirectorio::parseFile(FILE* dirAct, int offsetDocs){
 
 		}
 
-
 	}
 
 }
-bool ParserDirectorio::isCurrOrParentDir(const string& name)
-{
+bool ParserDirectorio::isCurrOrParentDir(const string& name) {
 	return name == CURR_DIR_STR || name == PARENT_DIR_STR;
 }
 
 ParserDirectorio::~ParserDirectorio() {
 
-	delete(parser);
+	delete (parser);
 
 }
 
